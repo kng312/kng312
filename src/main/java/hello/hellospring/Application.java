f8ab9e307ab7f43831cb98486cf2c559889b53eb
kng312 <kng312@naver.com>
2022-05-02 14:57:19 +0900

    spring study

---
+++ src/main/java/hello/hellospring/Application.java
@@ -0,0 +1,18 @@
+package hello.hellospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+        System.out.println("---------------------------------------------------");
+        System.out.println("------------------------start----------------------");
+        System.out.println("------------------------start----------------------");
+        System.out.println("---------------------------------------------------");
+
+    }
+
+}
