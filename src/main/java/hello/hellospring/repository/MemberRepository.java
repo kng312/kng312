2b54e9c2d5ead171f4cb8eeeb77e80c7343c085c
sounguk <kng312@naver.com>
2022-05-05 21:48:10 +0900

    spring MVC

--- src/main/java/hello/hellospring/repository/MemberRepository.java
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -1,11 +1,13 @@
 package hello.hellospring.repository;
 
 import hello.hellospring.domain.Member;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
-public interface MemberInterface {
+@Repository
+public interface MemberRepository {
     Member save(Member member);
 
     Optional<Member> findById(Long id);
---
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -0,0 +1,48 @@
+package hello.hellospring.service;
+
+import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /*
+    * 회원 가입
+    * */
+
+    public Long join(Member member) {
+        //같은 이름이 있는 중복 회원은 안된다.
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /*
+    * 전체 회원 조회
+    * */
+
+    public List<Member> findMember() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
